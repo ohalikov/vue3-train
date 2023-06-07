ae118ffa16ec044251357924c331d2a7f63f4dab
ohalikov <olegway31@gmail.com>
2023-06-07 22:34:43 +0500

    add 2cohesion from model and inputs

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
 <template>
-  <button @click="addDislikes">dislike</button>
-  <button @click="addLikes">like</button>
   <div>
-    Количество лайков: <strong>{{ likes }}</strong>
-  </div>
-  <div>
-    Количество дизлайков: <strong>{{ dislikes }}</strong>
+    <form class="form">
+      <h4>Создание поста</h4>
+      <input
+        v-bind:value="title"
+        @input="inputTitle"
+        class="input"
+        type="text"
+        placeholder="Название"
+      />
+      <input
+        v-bind:value="body"
+        class="input"
+        type="text"
+        placeholder="Описание"
+      />
+      <button class="btn" @click="createPost">Создать</button>
+    </form>
+    <div class="post" v-for="post in posts">
+      <div><strong>Название поста:</strong> {{ post.title }}</div>
+      <div><strong>Описание поста:</strong> {{ post.body }}</div>
+    </div>
   </div>
 </template>
 
@@ -13,19 +28,58 @@
 export default {
   data() {
     return {
-      likes: 0,
-      dislikes: 0,
+      posts: [
+        { id: 1, title: 'Javascript1', body: 'Описание ящыка' },
+        { id: 2, title: 'Javascript2', body: 'Описание ящыка' },
+        { id: 3, title: 'Javascript3', body: 'Описание ящыка' },
+        { id: 4, title: 'Javascript4', body: 'Описание ящыка' },
+        { id: 5, title: 'Javascript5', body: 'Описание ящыка' },
+      ],
+      title: '',
+      body: '',
     };
   },
   methods: {
-    addLikes() {
-      this.likes += 1;
-    },
-    addDislikes() {
-      this.dislikes += 1;
+    createPost() {},
+    inputTitle(event) {
+      console.log(this);
+      this.title = event.target.value;
     },
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.app {
+  padding: 20px;
+}
+.post {
+  padding: 15px;
+  border: 2px solid teal;
+  margin-top: 15px;
+}
+.form {
+  display: flex;
+  flex-direction: column;
+}
+.input {
+  width: 100%;
+  border: 1px solid teal;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
+
+.btn {
+  margin-top: 15px;
+  align-self: flex-end;
+  padding: 10px 15px;
+  background: none;
+  color: teal;
+  border: 1px solid teal;
+}
+</style>
